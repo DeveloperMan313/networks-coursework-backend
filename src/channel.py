e98132f5c4263a3db826a9ace596f63d8a857558
DeveloperMan313 <053000voloshch@gmail.com>
2026-04-17 21:23:32 +0300

    Define Frame, MsgTX and MsgRX classes, add basic init/methods to Port_cha

--- src/channel.py
+++ src/channel.py
@@ -1,4 +1,6 @@
 from enum import Enum, auto
+from queue import Queue
+from typing import Literal
 
 from src.physical import PC_phy, Port_phy
 
@@ -41,6 +43,63 @@ class PFrameH(Enum):
     MAILEND = auto()
 
 
+class Frame:
+    _head: PFrameH
+    _data: int | None
+
+    def __init__(self, head: PFrameH, data: int | None = None):
+        if head == PFrameH.MAILDATA and data is None:
+            raise ValueError("MAILDATA frame should have data")
+        if data and not 0 <= data <= 255:
+            raise ValueError("invalid data")
+        self._head = head
+        self._data = data
+
+    @property
+    def head(self) -> PFrameH:
+        return self._head
+
+    @property
+    def data(self) -> int | None:
+        return self._data
+
+
+class MsgTX(Frame):
+    def __init__(
+        self,
+        head: Literal[
+            PFrameH.UPLINK, PFrameH.DOWNLINK, PFrameH.LINKACTIVE, PFrameH.MAILDATA
+        ],
+        data: int | None = None,
+    ):
+        super().__init__(head, data)
+
+
+class MsgRX(Frame):
+    def __init__(
+        self,
+        head: Literal[
+            PFrameH.UPLINK, PFrameH.DOWNLINK, PFrameH.LINKACTIVE, PFrameH.MAILDATA
+        ],
+        data: int,
+    ):
+        super().__init__(head, data)
+        if head != PFrameH.MAILDATA and data != 1 and data != 0:
+            raise ValueError(
+                f"{head.name} message should have data 1 (success) or 0 (fail)"
+            )
+
+
 class Port_cha(Port_phy):
     def __init__(self, name: str):
         super().__init__(name)
+        self.__state = PS_cha.INACTIVE
+        self.__timer: int = 0
+        self.__send_buffer: Queue[MsgTX] = Queue()
+        self.__receive_buffer: Queue[MsgRX] = Queue()
+
+    def enqueue_send_msg(self, msg: MsgTX):
+        self.__send_buffer.put(msg)
+
+    def get_received_msg(self) -> MsgRX:
+        return self.__receive_buffer.get(block=False)
