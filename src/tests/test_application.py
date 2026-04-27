1ff978da6732948d195d0bd5124b40c5e3632bb0
DeveloperMan313 <053000voloshch@gmail.com>
2026-04-28 01:09:06 +0300

    Add application layer string transfer test

--- src/tests/test_application.py
+++ src/tests/test_application.py
@@ -33,3 +33,39 @@ class TestPort_app(unittest.IsolatedAsyncioTestCase):
             await port2.channel_downlink()
         except RuntimeError:
             self.fail("Port2 should downlink successfully")
+
+    async def test_ports_uplink_send_receive_string_bidirectionally(self):
+        port1 = Port_app("port 1")
+        port2 = Port_app("port 2")
+        port1.connect(port2)
+
+        async def do_tick():
+            while not port1._has_response() and not port2._has_response():
+                port1.do_tick()
+                port2.do_tick()
+                await asyncio.sleep(0)
+
+        asyncio.create_task(do_tick())
+        try:
+            await port2.channel_uplink()
+        except RuntimeError:
+            self.fail("Port2 should uplink successfully")
+
+        string = "Hello world!"
+
+        asyncio.create_task(do_tick())
+        try:
+            await port1.send_str(string)
+        except RuntimeError:
+            self.fail("Port1 should send string successfully")
+
+        # port2 should receive string before port1 gets success message
+        self.assertEqual(
+            port2.get_received_str(),
+            string,
+            "Received string should be equal to sent string",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
